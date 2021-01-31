da50fec43ffc3d6f468c917aa9ee5e28a43f9db9
Sinan <sinan_aldefai@hotmail.com>
2021-01-31 02:02:48 +0100

    made custom event for event summary

--- week_planner/src/components/Main.vue
+++ week_planner/src/components/Main.vue
@@ -18,10 +18,11 @@
           @mouseup:time="endDrag"
           @mouseleave.native="cancelDrag"
         > 
-          <template v-slot:event="{ event, timed, eventSummary }">
+
+          <template v-slot:event="{ event, timed }">
             <div
               class="v-event-draggable"
-              v-html="eventSummary()"
+              v-html="customEventSummary(event)"
             ></div>
             <div
               v-if="timed"
@@ -108,6 +109,7 @@
           this.createStart = this.roundTime(mouse)
           this.createEvent = {
             name: `Event #${this.events.length}`,
+            category: 'patient',
             color: this.rndElement(this.colors),
             start: this.createStart,
             end: this.createStart,
@@ -116,6 +118,9 @@
 
           this.events.push(this.createEvent)
         }
+      },
+      customEventSummary(event){
+        return event.name;
       },
       extendBottom (event) {
         this.createEvent = event
@@ -191,32 +196,34 @@
             ? `rgba(${r}, ${g}, ${b}, 0.7)`
             : event.color
       },
-      getEvents ({ start, end }) {
-        const events = []
 
-        const min = new Date(`${start.date}T00:00:00`).getTime()
-        const max = new Date(`${end.date}T23:59:59`).getTime()
-        const days = (max - min) / 86400000
-        const eventCount = this.rnd(days, days + 20)
+      // getEvents ({ start, end }) {
+      //   const events = []
 
-        for (let i = 0; i < eventCount; i++) {
-          const timed = this.rnd(0, 3) !== 0
-          const firstTimestamp = this.rnd(min, max)
-          const secondTimestamp = this.rnd(2, timed ? 8 : 288) * 900000
-          const start = firstTimestamp - (firstTimestamp % 900000)
-          const end = start + secondTimestamp
+      //   const min = new Date(`${start.date}T00:00:00`).getTime()
+      //   const max = new Date(`${end.date}T23:59:59`).getTime()
+      //   const days = (max - min) / 86400000
+      //   const eventCount = this.rnd(days, days + 20)
 
-          events.push({
-            name: this.rndElement(this.names),
-            color: this.rndElement(this.colors),
-            start,
-            end,
-            timed,
-          })
-        }
+      //   for (let i = 0; i < eventCount; i++) {
+      //     const timed = this.rnd(0, 3) !== 0
+      //     const firstTimestamp = this.rnd(min, max)
+      //     const secondTimestamp = this.rnd(2, timed ? 8 : 288) * 900000
+      //     const start = firstTimestamp - (firstTimestamp % 900000)
+      //     const end = start + secondTimestamp
 
-        this.events = events
-      },
+      //     events.push({
+      //       name: this.rndElement(this.names),
+      //       color: this.rndElement(this.colors),
+      //       start,
+      //       end,
+      //       timed,
+      //     })
+      //   }
+
+      //   this.events = events
+      // },
+
       rnd (a, b) {
         return Math.floor((b - a + 1) * Math.random()) + a
       },
